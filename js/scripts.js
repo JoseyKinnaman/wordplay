36de79d66073acffe3e43cdd18dfe5ae46e609bc
Jozy Kinnaman <jozypants@gmail.com>
2020-03-30 19:27:55 -0700

    add js variables and forEach loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
 $(document).ready(function () {
-  $("#formOne").submit(function (event) {
+  var threePlus = [];
+  var newSentence;
+  $("#formOne").submit(function(event) {
     event.preventDefault();
+    threePlus = []
+    var sentence = $("input#sentence").val();
+    console.log(sentence);
+    var words = sentence.split(" ");
+    console.log(words);
+    words.forEach(function (word) {
+      if (word.length >= 3) {
+        threePlus.push(word);
+      }
+    })
+    newSentence = threePlus.reverse().join(" ");
+    $("#output").text(newSentence);
+   
+    
   });
 });
